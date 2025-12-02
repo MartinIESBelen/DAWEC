bd2d34e7ac8162d2e84dead0d4cf349cb0040173
Martín IES Belén <sierramartinlfm@gmail.com>
2025-12-03 00:25:15 +0100

    Ejercicio form event

---
+++ dawecl/Tema02/formRegistration/script.js
@@ -0,0 +1,9 @@
+const foodFlavors ={
+    Pizza:["barbacoa", "peperoni", "hawaiana"],
+    Burger:["double cheese", "diabola", "Bacon"],
+    Campero:["pollo", "tortilla", "completo"]
+}
+
+function validarName(){
+
+}
